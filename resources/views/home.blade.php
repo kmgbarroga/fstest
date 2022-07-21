9552bb8c84ade3e01cf2003fea24029f84a9cfd2
kmgbarroga <kmgbarroga@gmail.com>
2022-07-21 21:20:41 +0800

    complete-display-users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,10 +14,59 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <h4>Users</h4>
+                    <div id="table-container"></div>
+
                 </div>
             </div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('customjs')
+    <script>
+        $(document).ready(function(){
+            var table = document.createElement("table");
+            table.classList.add("table");
+            var headers = ['Name','Username','Phone','Company Name'];
+
+            // table headers
+            var tr = table.insertRow(-1);
+            for (var i = 0; i < headers.length; i++) {
+                var th = document.createElement("th");
+                th.innerHTML = headers[i];
+                tr.appendChild(th);
+            }
+
+            // create rows of data
+            // fetch from api
+            $.getJSON('https://jsonplaceholder.typicode.com/users', function(users) {
+                users.forEach(eachUser => {
+                    // insert row
+                    tr = table.insertRow(-1);
+
+                    // create cell for data
+                    var tabCell = tr.insertCell(-1);
+                    tabCell.innerHTML = eachUser["name"];
+
+                    var tabCell = tr.insertCell(-1);
+                    tabCell.innerHTML = eachUser["username"];
+
+                    var tabCell = tr.insertCell(-1);
+                    tabCell.innerHTML = eachUser["phone"];
+
+                    var tabCell = tr.insertCell(-1);
+                    tabCell.innerHTML = eachUser["company"]["name"];
+
+
+                });
+            });
+
+            // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+            var divContainer = document.getElementById("table-container");
+            divContainer.innerHTML = "";
+            divContainer.appendChild(table);
+        })
+    </script>
 @endsection
